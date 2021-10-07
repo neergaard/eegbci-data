2df7ed65fdd9ddb9745cb617369781579cded242
Alexander Neergaard Zahid <aneol@dtu.dk>
2021-10-07 14:03:25 +0200

    expose datamodule and dataset

--- eegbci/__init__.py
+++ eegbci/__init__.py
@@ -1,3 +1,6 @@
 from .fetch_data.download_data import download_eegbci
 
 from .preprocessing.process_data import process_eegbci
+
+from .datamodule.eegbci import EEGBCIDataModule
+from .datamodule.dataset import EEGBCIDataset
