1e494260b67832d4e21ec2a2ed1ab7f50cad22de
Alexander Neergaard Zahid <aneol@dtu.dk>
2021-10-07 14:03:47 +0200

    add datamodule and dataset

--- eegbci/datamodule/dataset.py
+++ eegbci/datamodule/dataset.py
@@ -1,11 +1,14 @@
 import argparse
 import logging
 import os
+import random
+from itertools import compress
 
 import numpy as np
 from h5py import File
 from joblib import Memory, delayed
 from sklearn import preprocessing
+from sklearn.model_selection import KFold, StratifiedKFold
 from torch.utils.data import Dataset
 
 from .utils import ParallelExecutor
@@ -28,7 +31,7 @@ def _initialize_record(filename=None, scaling=None, sequence_length=None):
         y = h5["targets/labels"] or h5["tasks/labels"]
         t = h5["targets/onehot"] or h5["tasks/onehot"]
         metadata = dict(h5.attrs)
-        subject_id = metadata["id"]
+        record_id = metadata["id"]
 
         N, C, T = X.shape
         K = t.shape[-1]
@@ -47,15 +50,16 @@ def _initialize_record(filename=None, scaling=None, sequence_length=None):
         else:
             sequence_counts = N - sequence_length + 1
 
-        index_to_record = [{"subject": subject_id, "idx": x} for x in range(sequence_counts)]
+        index_to_record = [{"record": record_id, "idx": x} for x in range(sequence_counts)]
 
     return dict(
-        metadata=(subject_id, metadata),
-        scaler=(subject_id, scaler),
-        class_indices=(subject_id, class_indices),
-        class_counts=(subject_id, class_counts),
-        sequence_counts=(subject_id, sequence_counts),
+        metadata=(record_id, metadata),
+        scaler=(record_id, scaler),
+        class_indices=(record_id, class_indices),
+        class_counts=(record_id, class_counts),
+        sequence_counts=(record_id, sequence_counts),
         index_to_record=index_to_record,
+        data_shape=(N, C, T),
     )
 
 
@@ -64,7 +68,8 @@ class EEGBCIDataset(Dataset):
         self,
         data_dir=None,
         balanced_sampling=None,
-        cv_params=None,
+        cv=None,
+        cv_idx=None,
         eval_ratio=None,
         max_eval_records=None,
         n_channels=None,
@@ -72,21 +77,26 @@ class EEGBCIDataset(Dataset):
         n_records=None,
         scaling=None,
         sequence_length=None,
+        *args,
+        **kwargs,
     ):
         super().__init__()
         self.data_dir = data_dir
         self.balanced_sampling = balanced_sampling
-        self.cv_params = cv_params
+        self.cv = cv
+        self.cv_idx = cv_idx
         self.eval_ratio = eval_ratio
         self.max_eval_records = max_eval_records
         self.n_channels = n_channels
         self.n_jobs = n_jobs
         self.n_records = n_records
         self.scaling = scaling
+        self.kf = KFold(n_splits=self.cv) if self.cv > 1 else None
         self.sequence_length = (
             sequence_length if isinstance(sequence_length, str) and sequence_length == "full" else int(sequence_length)
         )
         self.records = sorted(os.listdir(self.data_dir))[: self.n_records]
+        self.n_records = self.n_records or len(self.records)
 
         # Get information about data
         logger.info(f"Prefetching study metadata using {self.n_jobs} workers:")
@@ -98,13 +108,14 @@ class EEGBCIDataset(Dataset):
             )
             for record in self.records
         )
-        logger.info("Prefetching finished")
+        logger.info("Prefetching finished.")
         self.class_counts = sum([s["class_counts"][1] for s in sorted_data])
         self.class_indices = dict([s["class_indices"] for s in sorted_data])
         self.index_to_record = [sub for s in sorted_data for sub in s["index_to_record"]]
         self.metadata = dict([s["metadata"] for s in sorted_data])
         self.scalers = dict([s["scaler"] for s in sorted_data])
         self.sequence_counts = dict([s["sequence_counts"] for s in sorted_data])
+        self.data_shape = sorted_data[0]["data_shape"][1:]
 
     def __len__(self):
         return sum(self.sequence_counts.values())
@@ -131,7 +142,7 @@ class EEGBCIDataset(Dataset):
             )
             current_sequence = slice(current_sequence_start, current_sequence_start + self.sequence_length)
         else:
-            current_subject = self.index_to_record[idx]["subject"]
+            current_subject = self.index_to_record[idx]["record"]
             if isinstance(self.sequence_length, str) and self.sequence_length == "full":
                 current_sequence = slice(None)
             else:
@@ -159,10 +170,104 @@ class EEGBCIDataset(Dataset):
 
         return x, t, current_subject, current_sequence
 
+    def _shuffle_records(self):
+        random.shuffle(self.records)
+
+    def _split_data(self):
+        self._shuffle_records()
+        if self.kf:
+            self.train_idx, self.eval_idx = list(self.kf.split(range(self.n_records)))[self.cv_idx]
+        else:
+            n_eval = min(int(self.n_records * self.eval_ratio), self.max_eval_records)
+            self.train_idx = np.arange(n_eval, self.n_records)
+            self.eval_idx = np.arange(0, n_eval)
+        self.train_data = EEGBCISubset(self, self.train_idx, balanced_sampling=self.balanced_sampling, name="train")
+        self.eval_data = EEGBCISubset(self, self.eval_idx, name="eval")
+
+        return self.train_data, self.eval_data
+
+    def __str__(self):
+        return f"""
+
+EEGBCI Dataset
+==============
+
+Parameters:
+------------------------------------------------------------------------------
+\tBalanced sampling:        {self.balanced_sampling}
+\tCross-validation folds:   {self.cv}
+\tCurrent CV fold:          {self.cv_idx}
+\tData directory:           {self.data_dir}
+\tEval ratio:               {self.eval_ratio}
+\tNumber of channels:       {self.data_shape[0]}
+\tNumber of records:        {self.n_records}
+\tNumber of sequences:      {len(self)}
+\tScaling type:             {self.scaling}
+\tSequence length:          {self.sequence_length * 5} min
+==============================================================================
+"""
+
+    @staticmethod
+    def add_dataset_specific_args(parent_parser):
+        # fmt: off
+        parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
+        dataset_group = parser.add_argument_group("dataset")
+        dataset_group.add_argument("-d", "--data_dir", default="./data/processed", type=str, help="Location of H5 dataset files.")
+        dataset_group.add_argument("--balanced_sampling", default=None, action="store_true", help="Whether to balance batches.")
+        dataset_group.add_argument("--cv", default=1, type=int, help="Number of CV folds.")
+        dataset_group.add_argument("--cv_idx", default=0, type=int, help="Specific CV fold index.")
+        dataset_group.add_argument("--eval_ratio", default=0.1, type=float, help="Ratio of validation subjects.")
+        dataset_group.add_argument("--max_eval_records", default=100, type=int, help="Max. number of subjects to use for eval.")
+        dataset_group.add_argument("--n_channels", default=None, type=int, help="Number of applied EEG channels.")
+        dataset_group.add_argument("--n_jobs", default=-1, type=int, help="Number of parallel jobs to run for prefetching.")
+        dataset_group.add_argument("--n_records", default=None, type=int, help="Total number of records to use.")
+        dataset_group.add_argument("--scaling", default="robust", choices=["robust", "standard"], help="How to scale EEG data.")
+        dataset_group.add_argument("--sequence_length", default=10, help="Number of sequences in each batch element.")
+        # fmt: on
+        return parser
+
 
 class EEGBCISubset(Dataset):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, dataset, record_indices, balanced_sampling=False, name=None):
+        self.dataset = dataset
+        self.record_indices = record_indices
+        self.balanced_sampling = balanced_sampling
+        self.name = name
+        self.subset_records = [self.dataset.records[idx].split(".")[0] for idx in self.record_indices]
+        self.sequence_indices = self.__get_subset_indices()
+
+    def __get_subset_indices(self):
+        records = set(self.subset_records)
+        t = list(map(lambda x: x["record"] in records, self.dataset.index_to_record))
+        return list(compress(range(len(t)), t))
+
+    def __len__(self):
+        return len(self.sequence_indices)
+
+    def __getitem__(self, idx):
+        return self.dataset[self.sequence_indices[idx]]
+
+    def __str__(self):
+        return f"""
+
+EEGBCI Subset
+==============
+
+Parameters:
+------------------------------------------------------------------------------
+\tBalanced sampling:        {self.dataset.balanced_sampling}
+\tCross-validation folds:   {self.dataset.cv}
+\tCurrent CV fold:          {self.dataset.cv_idx}
+\tData directory:           {self.dataset.data_dir}
+\tEval ratio:               {self.dataset.eval_ratio}
+\tNumber of channels:       {self.dataset.data_shape[0]}
+\tNumber of records:        {len(self.subset_records)}
+\tNumber of sequences:      {len(self)}
+\tScaling type:             {self.dataset.scaling}
+\tSequence length:          {self.dataset.sequence_length * 5} min
+\tSubset:                   {self.name}
+==============================================================================
+"""
 
 
 if __name__ == "__main__":
@@ -170,8 +275,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument("-d", "--data_dir", default="./data/processed", type=str, help="Location of H5 dataset files.")
     parser.add_argument("--balanced_sampling", default=None, action="store_true", help="Whether to balance batches.")
-    parser.add_argument("--n_cv", default=None, type=int, help="Number of CV folds.")
-    parser.add_argument("--cv_idx", default=None, type=int, help="Specific CV fold index.")
+    parser.add_argument("--cv", default=1, type=int, help="Number of CV folds.")
+    parser.add_argument("--cv_idx", default=0, type=int, help="Specific CV fold index.")
     parser.add_argument("--eval_ratio", default=0.1, type=float, help="Ratio of validation subjects.")
     parser.add_argument("--max_eval_records", default=100, type=int, help="Max. number of subjects to use for eval.")
     parser.add_argument("--n_channels", default=None, type=int, help="Number of applied EEG channels.")
@@ -180,10 +285,15 @@ if __name__ == "__main__":
     parser.add_argument("--scaling", default="robust", choices=["robust", "standard"], help="How to scale EEG data.")
     parser.add_argument("--sequence_length", default=10, help="Number of sequences in each batch element.")
     args = parser.parse_args()
-    args.cv_params = dict(cv_idx=args.cv_idx, n_cv=args.n_cv)
-    del args.cv_idx, args.n_cv
 
     # Test dataset
     ds = EEGBCIDataset(**vars(args))
-    print(len(ds))
+    print(ds)
     # next(iter(ds))
+
+    # Test splitting of data
+    train_data, eval_data = ds._split_data()
+    assert len(train_data) > 0 and len(eval_data) > 0
+    print(train_data)
+    print(eval_data)
+    print("Fin")
--- eegbci/datamodule/eegbci.py
+++ eegbci/datamodule/eegbci.py
@@ -2,59 +2,92 @@ import argparse
 
 from sklearn import preprocessing
 from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader
 
 from eegbci import download_eegbci, process_eegbci
 from eegbci.datamodule.dataset import EEGBCIDataset
 
 
 class EEGBCIDataModule(LightningDataModule):
-    def __init__(self, data_dir=None, subjects=None, processing_kwargs=None):
+    def __init__(self, processing_kwargs=None, **dataset_kwargs):
         super().__init__()
-        self.data_dir = data_dir
-        self.subjects = subjects
+        self.dataset_kwargs = dataset_kwargs
         self.processing_kwargs = processing_kwargs
 
     def prepare_data(self):
-        if self.prepare_data:
-            raw_dir = self.processing_kwargs["raw_dir"]
-            fs = self.processing_kwargs["fs"]
-            tmin = self.processing_kwargs["tmin"]
-            tmax = self.processing_kwargs["tmax"]
-            freq_band = self.processing_kwargs["freq_band"]
-            download_eegbci(raw_dir, self.subjects)
-            process_eegbci(raw_dir, self.data_dir, fs, tmin, tmax, self.subjects, freq_band)
+        raw_dir = self.processing_kwargs["raw_dir"]
+        fs = self.processing_kwargs["fs"]
+        tmin = self.processing_kwargs["tmin"]
+        tmax = self.processing_kwargs["tmax"]
+        freq_band = self.processing_kwargs["freq_band"]
+        n_records = self.dataset_kwargs["n_records"]
+        data_dir = self.dataset_kwargs["data_dir"]
+        overwrite = self.processing_kwargs["overwrite"]
+        download_eegbci(raw_dir, n_records)
+        if overwrite or not any([p.endswith(".h5") for p in os.listdir(data_dir)]):
+            process_eegbci(raw_dir, data_dir, fs, tmin, tmax, n_records, freq_band)
 
     def setup(self, stage=None):
 
         # Partition data
         if stage == "fit" or stage is None:
-            self.dataset = EEGBCIDataset(self.raw_dir)
-            self.train_data, self.eval_data = self.dataset.split_data()
+            self.dataset = EEGBCIDataset(**self.dataset_kwargs)
+            self.train_data, self.eval_data = self.dataset._split_data()
 
         if stage == "test":
-            raise NotImplementedError
+            self.test_data = self.eval_data
 
     def train_dataloader(self):
-        pass
+        return DataLoader(
+            self.train_data,
+            batch_size=self.train_data.batch_size,
+            shuffle=True,
+            num_workers=self.train_data.n_workers,
+            pin_memory=True,
+        )
 
     def val_dataloader(self):
-        pass
+        return DataLoader(
+            self.eval_data,
+            batch_size=self.eval_data.batch_size,
+            shuffle=False,
+            num_workers=self.eval_data.n_workers,
+            pin_memory=True,
+        )
 
     def test_dataloader(self):
-        pass
+        return DataLoader(
+            self.eval_data,
+            batch_size=self.eval_data.batch_size,
+            shuffle=False,
+            num_workers=self.eval_data.n_workers,
+            pin_memory=True,
+        )
+
+    @staticmethod
+    def add_argparse_args(parent_parser):
+        return EEGBCIDataset.add_dataset_specific_args(parent_parser)
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument("-d", "--data_dir", default="./data/processed", help="Location of H5 dataset files.")
+    parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument("--raw_dir", default="./data", help="Location of raw EDF files.")
-    parser = EEGBCI.add_argparse_args(parser)
+    parser.add_argument("--overwrite", action="store_true", help="Overwrite H5 files in directory.")
+    parser = EEGBCIDataset.add_dataset_specific_args(parser)
     args = parser.parse_args()
 
     # Preprocessing arguments
-    processing_kwargs = dict(raw_dir="./data", fs=128.0, tmin=-1.0, tmax=4.0, freq_band=[0.5, 35.0],)
+    processing_kwargs = dict(
+        raw_dir="./data", fs=128.0, tmin=-1.0, tmax=4.0, freq_band=[0.5, 35.0], overwrite=args.overwrite
+    )
 
     # Test datamodule
-    eegbci = EEGBCIDataModule("data/processed", 12, processing_kwargs)
+    eegbci = EEGBCIDataModule(processing_kwargs=processing_kwargs, **vars(args))
     eegbci.prepare_data()
+    eegbci.setup("fit")
+    eegbci.setup("test")
+
+    print(eegbci.train_data)
+    print(eegbci.eval_data)
+    print(eegbci.test_data)
