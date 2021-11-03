dec6c2b4b8ff4eb8807d54932ca6bc79d0e4b539
aneol <aneol@dtu.dk>
2021-11-03 15:06:00 +0100

    update build files

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="eegbci",
-    author="Alexander Neergaard Zahid, PhD",
-    version="0.1.0",
-    package_dir={"": "eegbci"},
-    packages=find_packages(where="eegbci"),
-    python_requires=">=3.9, <4",
-    install_requires=["h5py", "mne", "scikit-learn"],
-)
+setup()
