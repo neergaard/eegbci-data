0228be1262a8f3b4d5d6c9da438c24a7c85c5b97
Alexander Neergaard Olesen <alexander.neergaard@gmail.com>
2022-01-10 14:35:31 +0100

    remake pipeline; saves individual exp. runs.; fix number of classes to 9;

--- eegbci/preprocessing/process_eegbci.py
+++ eegbci/preprocessing/process_eegbci.py
@@ -4,6 +4,7 @@ import os
 import random
 import sys
 from time import time
+from functools import partial
 
 import h5py
 import mne
@@ -15,60 +16,62 @@ COHORT = "eegbci"
 logger = logging.getLogger("mne")
 
 
-def process_subject_fn(data_dir, fs, tmin, tmax, freq_band, event_id, runs):
-    def _process_subject(subject):
-        # raw_fnames = fetch_subject(subject, data_dir, runs=runs)
-        raw_fnames = mne.datasets.eegbci.load_data(subject, runs, data_dir)
-        raws_list = []
-        epochs_list = []
-        events_list = []
-        for fname in raw_fnames:
-            current_run = int(os.path.basename(fname).split(".")[0][-2:])
-            raw = mne.io.read_raw_edf(fname, preload=True, verbose=False)
-            mne.datasets.eegbci.standardize(raw)
+def process_subject_fn(subject, runs, data_dir, fs, tmin, tmax, freq_band, event_id):
+    # def _process_subject(subject):
+    # raw_fnames = fetch_subject(subject, data_dir, runs=runs)
+    raw_fnames = mne.datasets.eegbci.load_data(subject, runs, data_dir)
+    raws_list = []
+    epochs_list = []
+    events_list = []
+    for fname in raw_fnames:
+        current_run = int(os.path.basename(fname).split(".")[0][-2:])
+        raw = mne.io.read_raw_edf(fname, preload=True, verbose=False)
+        mne.datasets.eegbci.standardize(raw)
 
-            # Extract events
-            if current_run in [1, 2]:
-                event_id_to_annotation = {"T0": 0, "T1": 1, "T2": 2}
-            elif current_run in [3, 7, 11]:
-                event_id_to_annotation = {"T0": 0, "T1": 1, "T2": 2}
-            elif current_run in [4, 8, 12]:
-                event_id_to_annotation = {"T0": 0, "T1": 5, "T2": 6}
-            elif current_run in [5, 9, 13]:
-                event_id_to_annotation = {"T0": 0, "T1": 3, "T2": 4}
-            elif current_run in [6, 10, 14]:
-                event_id_to_annotation = {"T0": 0, "T1": 7, "T2": 8}
-            events, _ = mne.events_from_annotations(raw, event_id=event_id_to_annotation)
+        # Extract events
+        if current_run in [1, 2]:
+            event_id_to_annotation = {"T0": 0, "T1": 1, "T2": 2}
+        elif current_run in [3, 7, 11]:
+            event_id_to_annotation = {"T0": 0, "T1": 1, "T2": 2}
+        elif current_run in [4, 8, 12]:
+            event_id_to_annotation = {"T0": 0, "T1": 5, "T2": 6}
+        elif current_run in [5, 9, 13]:
+            event_id_to_annotation = {"T0": 0, "T1": 3, "T2": 4}
+        elif current_run in [6, 10, 14]:
+            event_id_to_annotation = {"T0": 0, "T1": 7, "T2": 8}
+        events, _ = mne.events_from_annotations(raw, event_id=event_id_to_annotation)
 
-            # Resample and filter data
-            raw, events = raw.resample(fs, events=events)
-            iir_params = {"ftype": "butterworth", "order": 2, "output": "sos"}
-            raw.filter(freq_band[0], freq_band[1], method="iir", iir_params=iir_params)
+        # Resample and filter data
+        raw, events = raw.resample(fs, events=events)
+        iir_params = {"ftype": "butterworth", "order": 2, "output": "sos"}
+        raw.filter(freq_band[0], freq_band[1], method="iir", iir_params=iir_params)
 
-            # Epoch data
-            # picks = mne.pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude="bads")
-            if current_run in [1, 2]:
-                epochs = mne.make_fixed_length_epochs(raw, duration=tmax - tmin, preload=True, proj=False, id=0)
-                epochs.event_id = event_id
-            else:
-                epochs = mne.Epochs(
-                    raw, events, event_id, tmin, tmax, proj=False, baseline=None, preload=True, on_missing="ignore",
-                )
-                epochs = epochs.crop(tmin=tmin, tmax=tmax, include_tmax=False)
+        # Epoch data
+        # picks = mne.pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude="bads")
+        if current_run in [1, 2]:
+            epochs = mne.make_fixed_length_epochs(raw, duration=tmax - tmin, preload=True, proj=False, id=0)
+            epochs.event_id = event_id
+        else:
+            epochs = mne.Epochs(
+                raw, events, event_id, tmin, tmax, proj=False, baseline=None, preload=True, on_missing="ignore",
+            )
+            epochs = epochs.crop(tmin=tmin, tmax=tmax, include_tmax=False)
 
-            raws_list.append(raw)
-            epochs_list.append(epochs)
-            events_list.append(events)
+        raws_list.append(raw)
+        epochs_list.append(epochs)
+        events_list.append(events)
 
-        return raws_list, events_list, mne.concatenate_epochs(epochs_list)
-
-    return _process_subject
+    return raws_list, events_list, mne.concatenate_epochs(epochs_list)
 
 
-def write_h5(subject, epochs, output_dir):
+# return _process_subject
+
+
+def write_h5(subject, run_nr, epochs, output_dir):
     # Create subject ID and filename
     subject_id = f"S{subject:03}"
-    filename = os.path.join(output_dir, subject_id + ".h5")
+    run_id = f"R{run_nr:02}"
+    filename = os.path.join(output_dir, subject_id + run_id + ".h5")
     os.makedirs(output_dir, exist_ok=True)
 
     # Create file
@@ -77,10 +80,12 @@ def write_h5(subject, epochs, output_dir):
         # Write raw sensor data and labels in chunks
         logger.info(f"Writing signals and targets to file: {filename}")
         data = epochs.get_data()
+        N, C, T = epochs.get_data().shape
+        K = len(epochs.event_id)
         labels = epochs.events[:, -1].reshape(-1, 1)
-        labels_onehot = OneHotEncoder(sparse=False, dtype=int).fit_transform(labels)
-        N, C, T = data.shape
-        _, K = labels_onehot.shape
+        labels_onehot = OneHotEncoder(sparse=False, dtype=int, categories=np.arange(K)[np.newaxis]).fit_transform(
+            labels
+        )
         f.create_dataset("signals", (N, C, T), data=data, chunks=(1, C, T))
         f.create_dataset("targets/labels", (N, 1), data=labels, chunks=(1, 1))
         f.create_dataset("targets/onehot", (N, K), data=labels_onehot, chunks=(1, K))
@@ -94,6 +99,7 @@ def write_h5(subject, epochs, output_dir):
         f.attrs["id"] = subject_id
         f.attrs["fs"] = epochs.info["sfreq"]
         f.attrs["n_channels"] = C
+        f.attrs["n_classes"] = K
         f.attrs["n_events"] = [sum(epochs.events[:, -1] == v) for k, v in epochs.event_id.items()]
         f.attrs["n_epochs"] = N
         f.attrs["n_timepoints"] = T
@@ -120,23 +126,31 @@ def preprocess_eegbci(data_dir, output_dir=None, fs=128, tmin=-1.0, tmax=4.0, su
         "hands/imagined/both": 7,
         "feet/imagined": 8,
     }
-    runs = list(range(1, 15))
+    runs = list(range(3, 15))  # We skip baselines
 
     # Submit arguments to processing function
-    process_subject = process_subject_fn(data_dir, fs, tmin, tmax, freq_band, event_id, runs)
+    # process_subject = process_subject_fn(data_dir, fs, tmin, tmax, freq_band, event_id, runs)
+    process_subject = partial(
+        process_subject_fn, data_dir=data_dir, fs=fs, tmin=tmin, tmax=tmax, freq_band=freq_band, event_id=event_id
+    )
 
     # Run over single subjects
     start = time()
-    process_ok = 0
+    ok_subjects = 0
     for subject in subjects_to_run:
         logger.info(f"S{subject:03}")
         logger.info("---------------------------")
-        raw, events, epochs = process_subject(subject)
+        process_ok = 0
+        for run_nr in runs:
+            raw, events, epochs = process_subject(subject=subject, runs=run_nr)
 
-        # Save subject data to disk
-        out = write_h5(subject, epochs, save_dir)
-        if not out:
-            process_ok += 1
+            # Save subject data to disk
+            out = write_h5(subject, run_nr, epochs, save_dir)
+            if not out:
+                process_ok += 1
+
+        if process_ok == len(runs):
+            ok_subjects += 1
 
     stop = time()
-    logger.info(f"Preprocessing finished. {process_ok} subjects written to disk in {stop - start :.3f} seconds.")
+    logger.info(f"Preprocessing finished. {ok_subjects} subjects written to disk in {stop - start :.3f} seconds.")
