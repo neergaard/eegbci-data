2a4290bdac2928b1790672b4d845428b61544df1
Alexander Neergaard Zahid <alexander.neergaard@gmail.com>
2022-02-11 10:38:33 +0100

    :sparkles: add logging to processing pipeline

--- eegbci/preprocess_data.py
+++ eegbci/preprocess_data.py
@@ -5,7 +5,11 @@ import sys
 from .preprocessing import preprocessing_fns
 
 
-logger = logging.getLogger("mne")
+logger = logging.getLogger("eegbci")
+logger.setLevel(logging.DEBUG)
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+logger.addHandler(stream_handler)
 
 
 AVAILABLE_DATASETS = set(preprocessing_fns.keys())
@@ -21,13 +25,19 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--data_dir", type=str, required=True, help="Path to EDF data.")
     parser.add_argument("-o", "--output_dir", type=str, default=None, help="Where to store H5 files.")
     parser.add_argument('-c', '--cohort', type=str, default='eegbci', help="Choice of preprocessing function (default 'eegbci').")
-    parser.add_argument("--fs", type=int, default=128, help="Desired resampling frequency.")
+    parser.add_argument("--fs", type=int, default=None, help="Desired resampling frequency.")
     parser.add_argument("--tmin", type=float, default=0., help="Start time of epochs relative to cue onset.")
     parser.add_argument("--tmax", type=float, default=4., help="End time of epochs relative to cue onset.")
     parser.add_argument("--subjects", type=int, nargs="+", default=None, help='Number of subjects to process. If None, all are processed.')
-    parser.add_argument('--freq_band', type=float, nargs="+", default=[0.5, 35.], help='Lower and upper frequencies for passband filter.')
+    parser.add_argument('--freq_band', type=float, nargs="+", default=None, help='Lower and upper frequencies for passband filter.')
+    parser.add_argument('--log', action='store_true')
     args = parser.parse_args()
     # fmt: on
+
+    if args.log:
+        file_handler = logging.FileHandler("logs/process_data.log", mode="w")
+        file_handler.setLevel(logging.DEBUG)
+        logger.addHandler(file_handler)
 
     logger.info(f'Usage: {" ".join([x for x in sys.argv])}\n')
     logger.info("Settings:")
@@ -38,4 +48,8 @@ if __name__ == "__main__":
         else:
             logger.info(f"{k:>15}\t{v}")
 
-    process_data(**vars(args))
+    if args.log:
+        args = vars(args)
+        args.pop("log")
+
+    process_data(**args)
