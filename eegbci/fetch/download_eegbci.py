81f150f2e5befec82391b7da32cc310d844e7518
Alexander Neergaard Zahid <alexander.neergaard@gmail.com>
2022-02-11 10:38:58 +0100

    :bug:fix n_first to 109 if None

--- eegbci/fetch/download_eegbci.py
+++ eegbci/fetch/download_eegbci.py
@@ -8,7 +8,7 @@ def fetch_subject(subject, path, runs=range(1, 15)):
     return mne.datasets.eegbci.load_data(subject, runs, path)
 
 
-def fetch_eegbci(out_dataset_folder, n_first=None):
+def fetch_eegbci(out_dataset_folder, n_first=109):
     """ Download the EEG Motor Movement/Imagery dataset (EEGBCI) from MNE."""
 
     # Make directory
